d5a9d755626acf18bc698b78be00d2200606401c
Lennart <l.nickel01@gmail.com>
2019-07-07 00:32:44 +0200

    added and implemented lastEat

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 const surveyRoot = document.getElementById("surveyRoot");
 
-const answers = {id: 0, name: "John Doe"};
+const answers = {id: 0, lastEatTime: "00:00"};
 
 function testStartup() {
     inflateForm("testButton.html");
@@ -11,7 +11,7 @@ function inflateForm(name) {
     let xHttp = new XMLHttpRequest();
     xHttp.onreadystatechange = function () {
         if (this.readyState == 4) {
-            if (this.status == 200){ surveyRoot.innerHTML = this.responseText; }
+            if (this.status == 200) { surveyRoot.innerHTML = this.responseText; }
             if (this.status == 404) { surveyRoot.innerHTML = "Survey not found."; }
         }
     };
@@ -20,7 +20,11 @@ function inflateForm(name) {
 }
 
 function setId() {
-    let id = document.getElementById("enterId").elements[0];
-    answers.id = id;
-    inflateForm("testButton.html");
+    answers.id = document.getElementById("enterId").elements[0];
+    inflateForm("lastEat.html");
+}
+
+function setLastEat() {
+    answers.lastEatTime = document.getElementById("lastEat").elements[0].value;
+    inflateForm("testButton.html")
 }
