844e882fc5ceac957cc0e29056fb537f0b806449
Lennart <l.nickel01@gmail.com>
2019-07-07 13:36:19 +0200

    rewrote logic to use central functions and central buttons

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ const surveyRoot = document.getElementById("surveyRoot");
 
 const answers = {id: 0, lastEatTime: "00:00"};
 
+let progress = 0;
+
+const formByIndex = ["testButton.html", "enterId.html", "lastEat.html"];
+
 function testStartup() {
     inflateForm("testButton.html");
 }
@@ -17,14 +21,44 @@ function inflateForm(name) {
     };
     xHttp.open("GET", "forms/" + name, true);
     xHttp.send();
+    setUpForm(progress);
 }
 
-function setId() {
-    answers.id = document.getElementById("enterId").elements[0];
-    inflateForm("lastEat.html");
+function nextForm(){
+    saveFormValues(progress);
+    progress += 1;
+    if (progress >= 1){
+        document.getElementById("btnPrev").style.visibility = "visible";
+    }
+    if (progress >= formByIndex.length-1){
+        document.getElementById("btnNext").style.visibility = "hidden";
+    }
+    inflateForm(formByIndex[progress]);
 }
 
-function setLastEat() {
-    answers.lastEatTime = document.getElementById("lastEat").elements[0].value;
-    inflateForm("testButton.html")
+function prevForm() {
+    saveFormValues(progress);
+    progress -= 1;
+    if (progress <= 0){
+        document.getElementById("btnPrev").style.visibility = "hidden";
+    }
+    if (progress < formByIndex.length-1){
+        document.getElementById("btnNext").style.visibility = "visible";
+    }
+    inflateForm(formByIndex[progress]);
+}
+
+function setUpForm(index){
+    switch (index) {
+        //setup your form
+        default: console.log("form " + progress + " needs no setup"); break;
+    }
+}
+
+function saveFormValues(index) {
+    switch (index) {
+        case 1: answers.id = document.getElementById("enterId").elements[0]; break;
+        case 2: answers.lastEatTime = document.getElementById("lastEat").elements[0].value; break;
+        default: console.log("Nothing to save for form " + index); break;
+    }
 }
