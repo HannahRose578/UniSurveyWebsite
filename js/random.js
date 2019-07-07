1e5679ec7f65ab4533b2400fcaa1db069817968c
HannahRose578 <hannah.rose578@gmail.com>
2019-07-07 13:29:27 +0100

    optimised randomiser

--- js/random.js
+++ js/random.js
@@ -1,6 +1,4 @@
 function getRandomNumber() {
-    var minNumber = 1; // The minimum number you want
-    var maxNumber = 100; // The maximum number you want
-    var randomnumber = Math.floor(Math.random() * (maxNumber + 1) + minNumber); // Generates random number
+    var randomnumber = Math.floor(Math.random() * (100 + 1) + 1); // Generates random number
     return randomnumber.toString();
 }
